fe75634103d8f86c85ef7394d24ed0c6221bbea2
Mateen <mateenabdullopato@gmail.com>
2024-04-03 00:24:10 +0000

    removed beams

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -9,14 +9,13 @@ import NavBar from './NavBar';
 import Services from './Services';
 import Subscribe from './Subscribe';
 import WorkingExperience from './WorkingExperience';
-import { BackgroundBeams } from './ui/Background-beams';
 import ContainerScroll from './ui/container-scroll-animation';
 
 const Home = () => {
   return (
     <div className="w-full overflow-hidden text-white">
       <NavBar />
-      <BackgroundBeams />
+  
       <HeroSection />
       <div className='z-10 text-slate-800 grid place-items-center'>
         {/* about feature is in the containerscroll */}
